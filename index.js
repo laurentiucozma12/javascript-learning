7f4e72cc2a382a75e3d08c5e5214d85ccf84eb2e
Lau <laurentiucozma12@gmail.com>
2024-01-23 17:45:43 +0200

    update

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext("2d");
 
-// var canvasWidth = window.innerWidth;
-// var canvasHeight = window.innerHeight;
+var canvasWidth = window.innerWidth;
+var canvasHeight = window.innerHeight;
 
-canvas.width = 500;
-canvas.height = 280;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+// canvas.width = 500;
+// canvas.height = 280;
 canvas.style.background = '#ff8';
 
 class Circle {
-    constructor(x, y, radius, color) {
+    constructor(x, y, radius, color, text) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
+        this.text = text;
     }
 
     draw(context) {
         context.beginPath();
+
+        context.strokeStyle = this.color;
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.font = "20px Arial";
+        context.fillText(this.text, this.x, this.y);
+        // context.strokeText(this.text, this.x, this.y);
+
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        context.lineWidth = 5;
+
         context.stroke();
+
         context.closePath();
     }
 }
-
-let circle1 = new Circle(75, 75, 50, 'red');
-circle1.draw(context);
 
 class Square {
     constructor(x, y, width, color) {
@@ -41,13 +53,32 @@ class Square {
     }
 }
 
-let square1 = new Square(150, 150, 10, 'blue');
-square1.draw(context);
+let all_circles = [];
+let all_squares = [];
 
+let createSprites = function(sprite) {
+    sprite.draw(context);
+}
 
+for (let i = 0; i < 10; i++) {
+    let random_x = Math.random() * canvasWidth;
+    let random_y = Math.random() * canvasHeight;
 
+    let my_circle = new Circle(random_x, random_y, 50, 'black', i);
+    all_circles.push(my_circle);
 
+    createSprites(all_circles[i]);
 
+    random_x = Math.random() * canvasWidth;
+    random_y = Math.random() * canvasHeight;
+}
 
+for (let i = 0; i < 10; i++) {
+    let random_x = Math.random() * canvasWidth;
+    let random_y = Math.random() * canvasHeight;
 
+    let my_square = new Square(random_x, random_y, 50, 'purple');
+    all_squares.push(my_square);
 
+    createSprites(all_squares[i]);
+}
