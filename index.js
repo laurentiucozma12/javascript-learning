11c82f7d6f0b3f6d244bbea6aa758cd6496778d2
Lau <laurentiucozma12@gmail.com>
2024-01-28 11:57:39 +0200

    update

--- index.js
+++ index.js
@@ -2,14 +2,30 @@
 // Time 09:14:37 - https://www.youtube.com/watch?v=lfmg-EJ8gm4
 
 const myBox = document.getElementById("myBox");
-const moveAmount = 100;
+const cucumber = document.getElementById("cucumber");
+const heart = document.getElementById("heart");
+const moveAmount = 15;
 let x = 0;
 let y = 0;
+
+myBox.style.top = `${y}px`;
+myBox.style.left = `${x}px`;
+myBox.style.width = `${x + 195}px`;
+myBox.style.height = `${y + 195}px`;
+xBox = myBox.style.top;
+yBox = myBox.style.top;
 
 document.addEventListener("keydown", event => {
     myBox.textContent = "😲";
     myBox.style.backgroundColor = "tomato";
 });
+
+class Person {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 document.addEventListener("keyup", event => {
     myBox.textContent = "😊";
